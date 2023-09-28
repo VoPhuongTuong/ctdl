474dca30da0e852e30575e0aff7287b976762565
Lenovo <Lenovo@DESKTOP-QHEQLES>
2023-09-28 19:50:59 +0700

    thêm

--- src/lab1/MyArray.java
+++ src/lab1/MyArray.java
@@ -20,11 +20,43 @@ public class MyArray {
 		return newArr;
 	}
 
-	
-	
+	public int count() {
+		int n = 0;
+		for (int i = 0; i < array.length; i++) {
+			boolean check = true;
+			for (int j = 0; j < i; j++) {
+				if (array[i] == array[j]) {
+					check = false;
+				}
+			}
+			if (check == true) {
+				n++;
+			}
+		}
+		return n;
+	}
+
+	public int[] removeDuplicates() {
+		int[] newArray = new int[count()];
+		int n = 0;
+		for (int i = 0; i < array.length; i++) {
+			boolean check = true;
+			for (int j = 0; j < i; j++) {
+				if (array[i] == array[j]) {
+					check = false;
+				}
+			}
+			if (check == true) {
+				newArray[n] = array[i];
+				n++;
+			}
+		}
+		return newArray;
+	}
+
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3 };
+		int[] arr = { 1, 2, 3, 3, 4, 5, 4, 3, 2, 7 };
 		MyArray newArr = new MyArray(arr);
-		System.out.println(Arrays.toString(newArr.mirror()));
+		System.out.println(Arrays.toString(newArr.removeDuplicates()));
 	}
 }
