ddb04d97061b75270e86b2b063876d6471f85ac9
Lenovo <Lenovo@DESKTOP-QHEQLES>
2023-09-29 10:40:19 +0700

    THÊM

--- src/lab1/MyCaesar.java
+++ src/lab1/MyCaesar.java
@@ -23,12 +23,47 @@ public class MyCaesar {
 		} else {
 			return c;
 		}
-
 	}
+		public String encrypt(String input) {
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < input.length(); i++) {
+				char x = input.charAt(i);
+				char y= encryptChar(x);
+				builder.append(y);
+			}
+			return builder.toString();		
+	}
+		
+		public char decryptChar(char c) {
+			int x = -2;
+			for (int i = 0; i < ALPHABET.length; i++) {
+				if (ALPHABET[i] == c) {
+					x = i;
+					break;
+				}
+			}
+			if (x!= -2) {
+				int y = (x -n) % 26;
+			if(y<0)
 
+					y = y + 26;
+				return ALPHABET[y];
+			} else {
+				return c;
+			}
+		}
+		public String decrypt(String input) {
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < input.length(); i++) {
+				char x = input.charAt(i);
+				char y = decryptChar(x);
+				builder.append(y);
+			}
+			return builder.toString();
+		}
 	public static void main(String[] args) {
-		MyCaesar task2 = new MyCaesar(3);
-		char x1 = task2.encryptChar('D');
+		MyCaesar task2 = new MyCaesar(4);
+	String x1 = task2.decrypt("HOALA");
 		System.out.println(x1);
 	}
 
